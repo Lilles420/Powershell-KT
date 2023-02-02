62d4dfd25eb182782f5080c84a50c18f917dec89
Lilles420 <115455132+Lilles420@users.noreply.github.com>
2023-02-02 11:22:40 +0200

    Add files via upload

---
+++ LillesKT2.ps1
@@ -0,0 +1,42 @@
+﻿$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+$csv = Import-csv $dir/MOCK_DATA.csv | Select-Object first_name,last_name,ip_address -skip 1
+new-item $dir\ip.txt
+$loopcount=0
+foreach($ip in $csv){
+    $lol= $ip.ip_address
+    $enim= $ip.first_name
+    $pnim= $ip.last_name
+    $tervenim =$enim + " " + $pnim + ","
+    $tim= $lol.Split(".")
+    $uus = ""
+    $count= 0
+    
+    if ($loopcount -le 99){
+    foreach($ipo in $tim)
+    {
+     $ipo =[int]$ipo
+     if ($ipo -ge 0 -and $ipo -le 255){
+     $count +=1
+     $uus+= [string]$ipo+"."
+     }
+     else {
+     $uus += [string]$ipo+"."
+     }
+     if ($count -eq 4) {
+     $uus = ""
+     break
+    }
+    }
+    }
+    else{break}
+    $count = 0
+    
+    if ($uus -ne ""){
+    $loopcount+=1
+    $uus= $uus.TrimEnd(".")
+    $ip = $tervenim +$uus
+       
+    $ip >> $dir\ip.txt}
+    write-host $loopcount
+}
